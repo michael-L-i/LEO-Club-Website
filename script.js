efacf3a4e5d66e0582de615775f9630d1da5efb7
Michael-L-i-1 <mli5@stanford.edu>
2025-06-16 23:41:28 -0700

    jun 6

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 /* 
- * INTRUCTIONS:
- * change numImages to number of images inside images folder
- * initially, images are in reverse order so we have to manually invert
- * simply add new images to the folder and increment numImages each time
+ * INSTRUCTIONS:
+ * Images are automatically discovered by checking which files exist
+ * Simply add new images to the folder with the naming pattern image1.png, image2.png, etc.
+ * No need to manually update numImages anymore!
 */
 
-var numImages = 51;
-const imagesRev = [];
-for (var i = 1; i <= numImages; i++) {
-    imagesRev.push("image" + i + ".png");
+// Function to check if an image exists
+async function imageExists(src) {
+    return new Promise((resolve) => {
+        const img = new Image();
+        img.onload = () => resolve(true);
+        img.onerror = () => resolve(false);
+        img.src = src;
+    });
 }
-images = imagesRev.reverse()
+
+// Function to discover all images
+async function discoverImages() {
+    const images = [];
+    let i = 1;
+    
+    while (true) {
+        const imageName = `image${i}.png`;
+        const imagePath = `images/${imageName}`;
+        
+        if (await imageExists(imagePath)) {
+            images.push(imageName);
+            i++;
+        } else {
+            break; // No more images found
+        }
+    }
+    
+    return images.reverse(); // Reverse to maintain your original order
+}
 
 // Function to create and append sections for each image
-function createImageSections() {
+async function createImageSections() {
     const container = document.getElementById('image-container');
+    const images = await discoverImages();
+    
+    console.log(`Found ${images.length} images`);
 
     images.forEach(image => {
         // Create a new section
@@ -23,7 +49,7 @@ function createImageSections() {
         // Create an img element
         const img = document.createElement('img');
         img.src = `images/${image}`;
-        img.alt = image;  // Optionally, set alt attribute for accessibility
+        img.alt = image;
         
         // Append the image to the section
         section.appendChild(img);
