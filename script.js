fddc0e748ad46bdccfd476941387babafeca267d
Michael Li <michaelli2005li@gmail.com>
2026-09-06 01:04:23 -0700

    sept 6th 2026 add images

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Load all images from the images folder in reverse order (newest first)
 const gallery = document.getElementById('gallery');
-const totalImages = 65;
+const totalImages = 74;
 
-// Loop in reverse order: 60, 59, 58, ... 1
+// Loop from the newest event image down to the oldest.
 for (let i = totalImages; i >= 1; i--) {
   const img = document.createElement('img');
   img.src = `images/image${i}.png`;
